04bc9021ba933ff27e15d0def251b08b744b2776
Stephen Crockett <crockett95@gmail.com>
2014-10-24 19:22:54 -0500

    Added PHPUnit tests for OAuthService
    These tests are basically a direct copy from the Twitter service
    in the original library by lusitanian. I just copied them over to
    get some basic test coverage of the class.

--- tests/OAuthServiceTest.php
+++ tests/OAuthServiceTest.php
@@ -1,12 +1,282 @@
 <?php
 
-use Crockett95\ProjectPlace\OAuthService;
+namespace Crockett95\ProjectPlace;
 
-class OAuthServiceTest extends PHPUnit_Framework_TestCase
+class OAuthServiceTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAutoloadIsWorking()
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     */
+    public function testConstructCorrectInterfaceWithoutCustomUri()
     {
-        $service = new OAuthService();
-        $this->assertTrue(!$service->getStatus());
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Service\\ServiceInterface', $service);
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     */
+    public function testConstructCorrectInstanceWithoutCustomUri()
+    {
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Service\\AbstractService', $service);
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     */
+    public function testConstructCorrectInstanceWithCustomUri()
+    {
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Uri\\UriInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Service\\AbstractService', $service);
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     */
+    public function testGetRequestTokenEndpoint()
+    {
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertSame(
+            'https://api.projectplace.com/initiate',
+            $service->getRequestTokenEndpoint()->getAbsoluteUri()
+        );
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getAuthorizationEndpoint
+     */
+    public function testGetAuthorizationEndpoint()
+    {
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertSame(
+            OAuthService::ENDPOINT_AUTHORIZE,
+            $service->getAuthorizationEndpoint()->getAbsoluteUri()
+        );
+
+        $this->assertSame(
+            'https://api.projectplace.com/authorize',
+            $service->getAuthorizationEndpoint()->getAbsoluteUri()
+        );
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getAccessTokenEndpoint
+     */
+    public function testGetAccessTokenEndpoint()
+    {
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertSame(
+            'https://api.projectplace.com/token',
+            $service->getAccessTokenEndpoint()->getAbsoluteUri()
+        );
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnNulledResponse()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(null));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnResponseNotAnArray()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('notanarray'));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnResponseCallbackNotSet()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('foo=bar'));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseRequestTokenResponse
+     */
+    public function testParseRequestTokenResponseThrowsExceptionOnResponseCallbackNotTrue()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(
+            'oauth_callback_confirmed=false'
+        ));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestRequestToken();
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseRequestTokenResponse
+     * @covers Crockett95\ProjectPlace\OAuthService::parseAccessTokenResponse
+     */
+    public function testParseRequestTokenResponseValid()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(
+            'oauth_callback_confirmed=true&oauth_token=foo&oauth_token_secret=bar'
+        ));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface'),
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Token\\StdOAuth1Token', $service->requestRequestToken());
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseThrowsExceptionOnError()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('error=bar'));
+
+        $token = $this->getMock('\\OAuth\\OAuth1\\Token\\TokenInterface');
+
+        $storage = $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface');
+        $storage->expects($this->any())->method('retrieveAccessToken')->will($this->returnValue($token));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $storage,
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestAccessToken('foo', 'bar', $token);
+    }
+
+    /**
+     * @covers Crockett95\ProjectPlace\OAuthService::__construct
+     * @covers Crockett95\ProjectPlace\OAuthService::getRequestTokenEndpoint
+     * @covers Crockett95\ProjectPlace\OAuthService::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseValid()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(
+            'oauth_token=foo&oauth_token_secret=bar'
+        ));
+
+        $token = $this->getMock('\\OAuth\\OAuth1\\Token\\TokenInterface');
+
+        $storage = $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface');
+        $storage->expects($this->any())->method('retrieveAccessToken')->will($this->returnValue($token));
+
+        $service = new OAuthService(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $storage,
+            $this->getMock('\\OAuth\\OAuth1\\Signature\\SignatureInterface')
+        );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth1\\Token\\StdOAuth1Token', $service->requestAccessToken('foo', 'bar', $token));
     }
 }
